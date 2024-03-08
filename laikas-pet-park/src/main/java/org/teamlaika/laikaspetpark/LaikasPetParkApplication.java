95c5c724747342c452157b554d4bffd905def7b3
trappmichael <142745435+trappmichael@users.noreply.github.com>
2024-03-07 20:00:29 -0600

    Initial Commit

---
+++ laikas-pet-park/src/main/java/org/teamlaika/laikaspetpark/LaikasPetParkApplication.java
@@ -0,0 +1,13 @@
+package org.teamlaika.laikaspetpark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaikasPetParkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaikasPetParkApplication.class, args);
+	}
+
+}
---
+++ laikas-pet-park/src/test/java/org/teamlaika/laikaspetpark/LaikasPetParkApplicationTests.java
@@ -0,0 +1,13 @@
+package org.teamlaika.laikaspetpark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaikasPetParkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
