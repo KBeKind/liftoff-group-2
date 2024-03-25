b5268a15fb402b3efa74940f79a978b53301e583
terryweis <weist2088@gmail.com>
2024-03-25 15:19:22 -0500

    Provider Controller Outline

--- laikas-pet-park/src/main/java/org/teamlaika/laikaspetpark/controllers/ProviderController.java
+++ laikas-pet-park/src/main/java/org/teamlaika/laikaspetpark/controllers/ProviderController.java
@@ -1,4 +1,37 @@
 package org.teamlaika.laikaspetpark.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.teamlaika.laikaspetpark.models.data.ProviderRepository;
+
+@Controller
+@RequestMapping("services")
 public class ProviderController {
+
+//    @Autowired
+//    private ServiceRepository serviceRepository;
+//
+//    @Autowired
+//    private ProviderRepository providerRepository;
+//
+//    // To display any services the user has already created
+//    @GetMapping
+//    public String displayServices(){}
+//
+//    @GetMapping("create")
+//    public String displayServiceForm(){}
+//
+//    @PostMapping("create")
+//    public String processServiceForm(){}
+//
+//    @GetMapping("delete")
+//    public String processDeleteServiceForm(){}
+//
+//    @GetMapping("edit")
+//    public String editServiceForm(){}
+//
 }
